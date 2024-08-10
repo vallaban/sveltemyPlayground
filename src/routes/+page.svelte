8daffa71392ef39dec904b30b4a725c0befb3e77
Vallaban <vallaban.m@gmail.com>
2024-08-10 11:25:50 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Modal from './Modal.svelte'; 
+    let showModal = false;
+    function openModal() 
+    { 
+        showModal = true;
+    }
+    function closeModal() 
+    { 
+        showModal = false;
+    } 
+    </script>
+     <button on:click={openModal}>Open Modal</button>
+    <Modal show={showModal} on:click={closeModal}> <h2>Modal Title</h2>
+    <p>This is the modal content.</p>
+     <button on:click={closeModal}>Close</button>
+    </Modal>
---
+++ src/routes/Modal.svelte
@@ -0,0 +1,28 @@
+<!-- Modal.svelte -->
+  <script>
+export let show = false;
+</script> 
+{#if show}
+<div class="modal-backdrop">
+     <div class="modal-content">
+         <slot></slot>
+</div> </div>
+{/if} 
+<style>
+.modal-backdrop {
+     position: fixed; 
+     top: 0;
+      left: 0;
+width: 100%; 
+height: 100%;
+background: rgba(0, 0, 0, 0.5); 
+display: flex;
+justify-content: center; 
+align-items: center;
+}
+ .modal-content {
+background: white;
+ padding: 1rem; 
+ border-radius: 8px;
+} 
+</style>
